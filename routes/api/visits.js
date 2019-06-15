d479f9a891eb7724c34ce7a1db12dfbceb752dfe
Andrew Paffel <51855279+paffela@users.noreply.github.com>
2019-06-15 11:36:13 -0500

    get the id and push the visit all in one

--- routes/api/visits.js
+++ routes/api/visits.js
@@ -5,16 +5,17 @@ module.exports = function(app){
 
     app.post('/api/visits', (req, res) => {
 
-        // Store the supplied visit data
+        // get the visit from the request
         visit = req.body;
-        visits.push(visit);
 
+        // increase the pollen count
         pollenRecived += visit.pollen;
 
-        res.status(201).json({'id':visits.length});
+        //push and get the id and push the visit all in one
+        res.status(201).json({'id':visits.push(visit)});
     });
+    //get method to see the arraylength in browser
     app.get('/api/get', (req, res) => {
         res.send("visits:" + visits.length);
     });
 }
-
